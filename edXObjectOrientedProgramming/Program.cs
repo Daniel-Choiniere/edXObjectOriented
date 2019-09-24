1e674431345f3d25fe4a809119afc8d4c4e8e3d9
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-23 21:43:03 -0400

    first commit

---
+++ edXObjectOrientedProgramming/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace edXObjectOrientedProgramming
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var Car1 = new Car();
+
+            // Using dot notation to call members on Car1
+            Car1.Color = "White";
+            Car1.Year = 2010;
+            Car1.Mileage = 11000;
+
+            Console.WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}, and has {Car1.Mileage} miles on it.");
+        }
+    }
+    public class Car
+    {
+        // Defining properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+    }
+}
