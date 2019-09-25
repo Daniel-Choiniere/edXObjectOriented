eb86c54f384de151e0e5025680146425eaec8261
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-25 18:15:03 -0400

    brought in code for mod1A. Practice adding constructors to a class and implementing static class variables

---
+++ edXObjectOrientedProgramming/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace edXObjectOrientedProgramming
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ mod1_B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mod1_B
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
