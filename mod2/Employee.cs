393098690cc9070722316aa56a941afe3cb35fe1
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-25 21:52:07 -0400

    added our Employee class with necessary properties

---
+++ mod2/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace mod2
+{
+    public class Employee
+    {
+        // 3 private member variables: employeeName, employeeBaseSalary, and employeeId
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+        // Public properties
+        public string Name
+        {
+            get
+            {
+                return employeeName;
+            }
+
+            set
+            {
+                employeeName = value;
+            }
+        }
+        public double BaseSalary
+        {
+            get
+            {
+                return employeeBaseSalary;
+            }
+
+            set
+            {
+                employeeBaseSalary = value;
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return employeeId;
+            }
+
+            set
+            {
+                employeeId = value;
+            }
+        }
+    }
+}
