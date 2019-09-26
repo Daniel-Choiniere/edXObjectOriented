9d09b17a6a4f57e388d254ecfc48ee2f1fd07830
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-25 22:57:12 -0400

    refactored code from class

---
+++ mod2_B_AbstractClasses/mod2_B_AbstractClasses/Employee.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace mod2
+{
+    abstract class Employee
+    {
+        // Constructor
+        public Employee(String name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.ID = employeeCount++;
+        }
+
+        // This method returns the employee's base salary
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        // This method returns the employee's name
+        public String getName()
+        {
+            return this.Name;
+        }
+
+        public int getEmployeeID()
+        {
+            return this.ID;
+        }
+
+        public String toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        // This method returns the employee's ID and Name and confirms that the employee is in the system
+        // Abstract method
+        public abstract String employeeStatus();
+
+        // 3 private member variables: employeeName, employeeBaseSalary, and employeeId
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+        // Creates integer variable called "employeeCount" and assigns value to 1
+        private static int employeeCount = 1;
+
+        // Public properties
+        public string Name
+        {
+            get
+            {
+                return employeeName;
+            }
+
+            set
+            {
+                employeeName = value;
+            }
+        }
+        public double BaseSalary
+        {
+            get
+            {
+                return employeeBaseSalary;
+            }
+
+            set
+            {
+                employeeBaseSalary = value;
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return employeeId;
+            }
+
+            set
+            {
+                employeeId = value;
+            }
+        }
+    }
+}
