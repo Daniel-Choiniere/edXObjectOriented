2ef0209e34ebe02e4b20382857663bcab3eedaf7
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-09-25 22:32:47 -0400

    built our BusinessEmployee, and TechnicalEmployee classes with methods

---
+++ mod2/BusinessEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mod2
+{
+    public class BusinessEmployee : Employee
+    {
+        // Creates double variable called "bonusBudget" and assigns value to 1000
+        public double bonusBudget = 1000;
+
+        // Calls upon base-class (Employee Class) constructor from within derived class (BusinessEmployee Class)
+        // Sets baseSalary to 50000 for all BusinessEmployee objects
+        public BusinessEmployee(String name) : base(name, 50000)
+        {
+        }
+
+        // This method returns the toString() method - which is the employee's ID number and name - and prints the bonus budget
+        // This method needs to use the override keyword to "override" the base class's method
+        public override String employeeStatus()
+        {
+            return toString() + " with a budget of " + this.bonusBudget;
+        }
+    }
+}
--- mod2/Employee.cs
+++ mod2/Employee.cs
@@ -3,10 +3,49 @@ namespace mod2
 {
     public class Employee
     {
+        // Constructor
+        public Employee(String name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.ID = employeeCount++;
+        }
+
+        // This method returns the employee's base salary
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        // This method returns the employee's name
+        public String getName()
+        {
+            return this.Name;
+        }
+
+        public int getEmployeeID()
+        {
+            return this.ID;
+        }
+
+        public String toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        // This method returns the employee's ID and Name and confirms that the employee is in the system
+        public virtual String employeeStatus()
+        {
+            return toString() + " is in the company's system";
+        }
+
         // 3 private member variables: employeeName, employeeBaseSalary, and employeeId
         private string employeeName;
         private double employeeBaseSalary;
         private int employeeId;
+
+        // Creates integer variable called "employeeCount" and assigns value to 1
+        private static int employeeCount = 1;
 
         // Public properties
         public string Name
